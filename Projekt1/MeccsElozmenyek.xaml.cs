8a07e33d0b9a784fb4e3802aa2acd9abe305002a
MeriLevente <meri.levente@students.jedlik.eu>
2023-11-02 01:08:11 +0100

    meccs eredmények filter hazai és vendég meccsekkel

--- Projekt1/MeccsElozmenyek.xaml.cs
+++ Projekt1/MeccsElozmenyek.xaml.cs
@@ -21,16 +21,13 @@ namespace Projekt1
     {
         ObservableCollection<Match> matches;
         ObservableCollection<string> teamNames;
+        bool windowFullyLoaded = false;
         public MeccsElozmenyek(ObservableCollection<Match> matches, ObservableCollection<string> teamNames)
         {
             InitializeComponent();
+            windowFullyLoaded = true;
             this.matches = matches;
             this.teamNames = teamNames;
-            ResultsUpdater();
-        }
-
-        public void ResultsUpdater()
-        {
             teamsCB.DataContext = teamNames;
             meccsekLB.DataContext = matches;
             meccsekLB.Items.Refresh();
@@ -48,16 +45,23 @@ namespace Projekt1
             });
         }
 
-        private void teamsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private ObservableCollection<Match> GetFilteredMatches()
         {
-            allTeamsRB.IsChecked = false;
             ObservableCollection<Match> filteredMatches = new ObservableCollection<Match>();
             matches.ToList().ForEach(x =>
             {
-               if (x.home == teamsCB.SelectedItem.ToString() || x.away == teamsCB.SelectedItem.ToString())
-                     filteredMatches.Add(x);
+                if (x.home == teamsCB.SelectedItem.ToString() || x.away == teamsCB.SelectedItem.ToString())
+                    filteredMatches.Add(x);
             });
-            meccsekLB.DataContext = filteredMatches;
+            return filteredMatches;
+        }
+        private void teamsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            allTeamsRB.IsChecked = false;
+            hazaiMeccsekCB.IsEnabled = vendegMeccsekCB.IsEnabled = true;
+            hazaiMeccsekCB.IsChecked = false;
+            vendegMeccsekCB.IsChecked = false;
+            meccsekLB.DataContext = GetFilteredMatches();
             meccsekLB.Items.Refresh();
         }
 
@@ -65,6 +69,81 @@ namespace Projekt1
         {
             meccsekLB.DataContext = matches;
             meccsekLB.Items.Refresh();
+            if (windowFullyLoaded) {
+                hazaiMeccsekCB.IsEnabled = vendegMeccsekCB.IsEnabled = false;
+                hazaiMeccsekCB.IsChecked = false;
+                vendegMeccsekCB.IsChecked = false;
+            }
+        }
+
+        private ObservableCollection<Match> GetHomeMatches()
+        {
+            ObservableCollection<Match> filteredHomeMatches = new ObservableCollection<Match>();
+            matches.ToList().ForEach(x => { if (x.home == teamsCB.SelectedItem.ToString()) { filteredHomeMatches.Add(x); }; });
+            return filteredHomeMatches;
+        }
+
+        private ObservableCollection<Match> GetAwayMatches()
+        {
+            ObservableCollection<Match> filteredAwayMatches = new ObservableCollection<Match>();
+            matches.ToList().ForEach(x => { if (x.away == teamsCB.SelectedItem.ToString()) { filteredAwayMatches.Add(x); }; });
+            return filteredAwayMatches;
+        }
+
+
+        private void hazaiMeccsekCB_Checked(object sender, RoutedEventArgs e)
+        {
+            if(windowFullyLoaded && !(bool)vendegMeccsekCB.IsChecked && !(bool)allTeamsRB.IsChecked)
+            {
+                meccsekLB.DataContext = GetHomeMatches();
+                meccsekLB.Items.Refresh();
+            }
+            else if(windowFullyLoaded)
+            {
+                meccsekLB.DataContext = GetFilteredMatches();
+                meccsekLB.Items.Refresh();
+            }
+        }
+
+        private void vendegMeccsekCB_Checked(object sender, RoutedEventArgs e)
+        {
+            if (windowFullyLoaded && !(bool)hazaiMeccsekCB.IsChecked && !(bool)allTeamsRB.IsChecked)
+            {
+                meccsekLB.DataContext = GetAwayMatches();
+                meccsekLB.Items.Refresh();
+            }
+            else if(windowFullyLoaded)
+            {
+                meccsekLB.DataContext = GetFilteredMatches();
+                meccsekLB.Items.Refresh();
+            }
+        }
+
+        private void hazaiMeccsekCB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if(windowFullyLoaded && (bool)vendegMeccsekCB.IsChecked && !(bool)allTeamsRB.IsChecked)
+            {
+                meccsekLB.DataContext = GetAwayMatches();
+                meccsekLB.Items.Refresh();
+            } else if (windowFullyLoaded && !(bool)allTeamsRB.IsChecked)
+            {
+                meccsekLB.DataContext=null;
+                meccsekLB.Items.Refresh();
+            }
+        }
+
+        private void vendegMeccsekCB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (windowFullyLoaded && (bool)hazaiMeccsekCB.IsChecked && !(bool)allTeamsRB.IsChecked)
+            {
+                meccsekLB.DataContext = GetHomeMatches(); ;
+                meccsekLB.Items.Refresh();
+            }
+            else if (windowFullyLoaded && !(bool)allTeamsRB.IsChecked)
+            {
+                meccsekLB.DataContext = null;
+                meccsekLB.Items.Refresh();
+            }
         }
     }
 }
